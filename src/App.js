c6be4655542149d31d942d5004b39f6824d2752c
Jose Daniel Az <josedanielxela@gmail.com>
2023-09-17 21:27:21 -0600

    Added bars for visualizing MySQL data

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       }
       
       // Aquí deberías obtener los datos de tu API
-      const response = await fetch(`http://localhost:4000/api/mongo/${selectedValue.value}`);
+      const response = await fetch(`http://localhost:4000/api/dynamo/${selectedValue.value}`);
       const data = await response.json();
       
       const labels = Object.keys(data);
@@ -75,6 +75,81 @@ function App() {
     }
   };
 
+// Barras Mysql
+
+  const [MysqlData, setMysqlData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'Datos 1',
+        backgroundColor: 'rgba(255, 72, 0, 1)',
+        borderColor: 'rgba(0, 0, 0, 1)',
+        borderWidth: 1,
+        data: [],
+      },
+    ],
+  });
+
+  const [MysqlOptions, setMysqlOptions] = useState({
+    scales: {
+      x: {
+        type: 'category',
+        labels: [],
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  });
+
+  const onMysql = async () => {
+    try {
+      if (!selectedValue) {
+        console.error('Ninguna opción seleccionada');
+        return;
+      }
+      
+      // Aquí deberías obtener los datos de tu API
+      const response = await fetch(`http://localhost:4000/api/mysql/${selectedValue.value}`);
+      const data = await response.json();
+      
+      const labels = Object.keys(data);
+      const values = Object.values(data);
+
+      // Actualiza dynamoData y dynamoOptions con los nuevos datos
+      setMysqlData({
+        labels: labels,
+        datasets: [
+          {
+            label: 'Datos 1',
+            backgroundColor: 'rgba(255, 72, 0, 1)',
+            borderColor: 'rgba(0, 0, 0, 1)',
+            borderWidth: 1,
+            data: values,
+          },
+        ],
+      });
+      
+      setMysqlOptions({
+        scales: {
+          x: {
+            type: 'category',
+            labels: labels,
+          },
+          y: {
+            beginAtZero: true,
+          },
+        },
+      });
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
+
+
   const [selectedValue, setSelectedValue] = useState(null);
 
   return (
@@ -82,7 +157,9 @@ function App() {
       <h1>Gráfica de Barras</h1>
       <ComboBox onChange={(selected) => setSelectedValue(selected)} />
       <button onClick={onDynamo}>Dynamo</button>
+      <button onClick={onMysql}>Mysql</button>
       <BarChart data={dynamoData} options={dynamoOptions} />
+      <BarChart data={MysqlData} options={MysqlOptions} />
     </div>
   );
 }
